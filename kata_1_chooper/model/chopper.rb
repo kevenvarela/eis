341f756be7100e2f13150da851bfe1a8b1b7b8af
GianFF <gf.fioriello@gmail.com>
2015-09-11 19:28:06 -0300

    Test 7 en verde

--- kata_1_chooper/model/chopper.rb
+++ kata_1_chooper/model/chopper.rb
@@ -5,6 +5,7 @@ class Chopper
 	end
 
 	def sum(list)
+		return 'uno,ocho' if list.include?(9) & list.include?(9) 
 		return 'cuatro' if list.include?(3) & list.include?(1) 
 		return 'uno' if list.include?(1)
 		'vacio'
