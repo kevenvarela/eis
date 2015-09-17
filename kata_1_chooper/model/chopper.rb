e9f39d0495877084dbf410a5b82b014bfafd1ed4
GianFF <gf.fioriello@gmail.com>
2015-09-17 20:35:25 -0300

    Refactor de metodo sum

--- kata_1_chooper/model/chopper.rb
+++ kata_1_chooper/model/chopper.rb
@@ -1,17 +1,34 @@
 class Chopper
 
+  def initialize
+    @hash = { nil => "vacio", 0 => "cero" , 1 => "uno" , 2 => "dos", 3 => "tres", 
+      4 => "cuatro", 5 => "cinco", 6 => "seis", 7 => "siete", 8 => "ocho", 9 => "nueve", 
+      10 => "demasiado grande"}
+  end
+
   def chop(n, array)
     return -1 if ! array.include?(n)
     array.index(n)
   end
 
   def sum(array)
-    return 'nueve,nueve' if array.include?(50) & array.include?(49) 
-    return 'demasiado grande' if array.include?(50) & array.include?(50) 
-    return 'uno,ocho' if array.include?(9) & array.include?(9) 
-    return 'cuatro' if array.include?(3) & array.include?(1) 
-    return 'uno' if array.include?(1)
-    'vacio'   
+    n = array.inject :+
+    armar(n)
+  end
+
+  def unidad(n)
+    n % 10
+  end
+  
+  def descena(n)
+    n / 10
+  end
+
+  def armar(n)
+    return @hash[n]                               if n == nil
+    return @hash[descena(n)]                      if n > 99
+    return @hash[unidad(n)]                       if n < 10
+    return @hash[descena(n)]+','+@hash[unidad(n)] if n > 9
   end
 
 end
