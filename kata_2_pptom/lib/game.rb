4615e04f1ab0cc6f921fe60b824de9a6992e5bed
GianFF <gf.fioriello@gmail.com>
2015-09-30 12:24:18 -0300

    Adding test of dynamic of a set and refactors

--- kata_2_pptom/lib/game.rb
+++ kata_2_pptom/lib/game.rb
@@ -1,9 +1,27 @@
 class Game
-  #[nil => 'Empate', true => 'Gana', false => 'Pierde']
+
+  def initialize
+  	@scores = {'p1' => 0, 'p2' => 0}
+  end
 
   def winner?(p1, p2)
-    return p1 if p1.element.vs p2.element
-    return p2 if p2.element.vs p1.element
+    if p1.element.vs p2.element
+      @scores['p1'] += 1
+      return p1 
+    end
+    if p2.element.vs p1.element
+      @scores['p2'] += 1
+      return p2
+    end
     nil
   end	
+
+  def winner_of_set?(p1, p2)
+  	hash = {'p1' => p1, 'p2' => p2, nil => nil}
+    hash[@scores.key 2] 
+  end
+
+  def start_set(p1, p2)
+  	@scores = {'p1' => 0, 'p2' => 0}
+  end
 end
--- kata_2_pptom/spec/game_spec.rb
+++ kata_2_pptom/spec/game_spec.rb
@@ -1,5 +1,4 @@
 =begin
-
 Esta segunda Kata consiste en implementar el famoso juego Piedra-Papel-Tijera con 
 algunas variaciones. No hace falta implementar toda una aplicación sino simplemente 
 el modelo de objetos.
@@ -15,22 +14,6 @@ de tres rondas. O sea, un jugador para ganar una partida tiene que ganar 2 ronda
 
 A diferencia de la kata anterior, en este caso el diseño de las pruebas queda 
 completamente a su cargo.
-
-A modo de guía les compartimos algunos ejemplos conceptuales.
-
-Ejemplo 1: dinámica de una ronda
-jugador1 juega piedra
-jugador2 juega papel
-entonces la ronda la gana jugador2
-
-
-Ejemplo 2: dinámica de una partida
-jugador1 gana la primera ronda
-jugador2 gana la segunda ronda
-entonces hasta el momento no hay ganador de la partida
-jugador2 gana la tercera ronda
-entonces la partida la gana jugador2
-
 =end
 
 require 'rspec' 
@@ -49,6 +32,13 @@ describe 'Game' do
   let(:player2) { Player.new }  
 
   describe "dynamic of a round" do
+
+=begin
+  Ejemplo 1: dinámica de una ronda
+  jugador1 juega piedra
+  jugador2 juega papel
+  entonces la ronda la gana jugador2
+=end
 
     it 'when player1 plays Piedra and player2 plays Papel, 
      the winner must be player2' do
@@ -77,6 +67,36 @@ describe 'Game' do
   	  player2.play(Tijera.new)
       expect(game.winner?(player1, player2)).to eq nil
     end
+  end
 
-   end
+  describe "dynamic of a set" do
+
+=begin
+  Ejemplo 2: dinámica de una partida
+  jugador1 gana la primera ronda
+  jugador2 gana la segunda ronda
+  entonces hasta el momento no hay ganador de la partida
+  jugador2 gana la tercera ronda
+  entonces la partida la gana jugador2
+=end
+    it 'when player 2 wins two rounds, should win player 2' do
+      game.start_set(player1, player2)
+
+      player1.play Tijera.new
+      player2.play Papel.new
+      expect(game.winner?(player1, player2)).to eq player1
+      # {p1 => 1 , p2 = 0}
+      player1.play Tijera.new
+      player2.play Piedra.new 
+      expect(game.winner?(player1, player2)).to eq player2
+	  # {p1 => 1 , p2 = 1}
+      expect(game.winner_of_set?(player1, player2)).to eq nil
+
+      player1.play Papel.new
+      player2.play Mono.new
+      expect(game.winner?(player1, player2)).to eq player2
+      # {p1 => 1 , p2 = 2}
+      expect(game.winner_of_set?(player1, player2)).to eq player2
+    end
+  end
 end
