19bff7d67bdca39f09678c7424ad343a62913043
GianFF <gf.fioriello@gmail.com>
2015-10-16 20:13:13 -0300

    Adding unitary test of Board

--- kata_3_batallanaval/spec/board_spec.rb
+++ kata_3_batallanaval/spec/board_spec.rb
@@ -1,5 +1,14 @@
 require 'rspec'
+require "board.rb"
 
 describe 'Board' do
 
+  let(:board) { Board.new }
+
+  describe '::initialize' do
+    
+    context 'initializes a Board' do
+      it { expect(board.class).to eq Board.itself }
+    end
+  end
 end
