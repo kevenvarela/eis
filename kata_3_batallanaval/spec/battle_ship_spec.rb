a026fe0e128da5b1c5d3bfd0676b49a4f35b3d7e
GianFF <gf.fioriello@gmail.com>
2015-10-16 19:20:09 -0300

    Starting again

--- kata_3_batallanaval/spec/battle_ship_spec.rb
+++ kata_3_batallanaval/spec/battle_ship_spec.rb
@@ -4,11 +4,23 @@ require_relative '../lib/board'
 
 describe 'BattleShip' do
 
-  describe "initializing" do
+  let(:battle_ship) { BattleShip.new([10,10]) } 
+  
+  describe '::initialize' do
 
-    it 'when passing a matrix [5,5] then a board will be initialize with 25 cells' do
-  	  battle_ship = BattleShip.new([5,5])
-  	  expect(battle_ship.board_size?).to eq 25
+    context "#board_size" do
+  	  let(:battle_ship) { BattleShip.new([5,5]) } 
+      it { expect(battle_ship.board_size?).to eq 25 }
+    end
+
+    context "#board_size" do
+      it { expect(battle_ship.board_size?).to eq 100 }
     end
   end
+
+  context "#set_ship" do
+  	it{ expect(battle_ship.set_ship('Submarine', 'vertically', [1,1])).
+  		to eq battle_ship.ship_in?([1,1]) }
+  end
+
 end
