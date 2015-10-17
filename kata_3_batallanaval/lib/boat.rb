f7da9d0fd863f1c24c51115602b39bf6ea4c5e78
GianFF <gf.fioriello@gmail.com>
2015-10-17 12:44:08 -0300

    Adding test of Boat #positions?

--- kata_3_batallanaval/lib/boat.rb
+++ kata_3_batallanaval/lib/boat.rb
@@ -1,10 +1,38 @@
 class Boat 
-  attr_reader :type, :orientation, :position
+  attr_reader :type, :orientation, :position, :types
 
   def initialize(type, orientation, position)
   	@type = type
   	@orientation = orientation
   	@position = position
+  	@types = {"Destroyer": 3, "Cruise": 2, "Submarine": 1}
+  end
+=begin
+  def positions?
+    return position if type == "Submarine"
+  	
+    res = [position]
+  	
+    if orientation == "horizontally"
+      end_x = position[0] + types[type.to_sym]
+
+  	  puts "------------------"
+  	  puts position[0]
+  	  puts "------------------"
+  	  puts types[type.to_sym]
+  	  puts "------------------"
+  	  puts end_x
+  	  puts "------------------"
+
+  	  position[0].upto end_x.to_i {|x| res+[position[x],position[1]]}
+  	else
+  	  end_y = position[1]+ types[type.to_sym]
+  	  position[1].upto end_y.to_i {|y| res+[position[1],position[y]]}
+    end
+  end 
+=end
+  def positions?
+    [position] if type == "Submarine"
   end
 
   def equal?(boat)
--- kata_3_batallanaval/spec/boat_spec.rb
+++ kata_3_batallanaval/spec/boat_spec.rb
@@ -3,17 +3,32 @@ require "boat.rb"
 
 describe 'Boat' do
 
-  let(:submarine) { Boat.new("Submarine", "vetically", [1,1]) }
-  let(:destroyer) { Boat.new("Destroyer", "vetically", [1,1]) }
-  let(:cruise)    { Boat.new("Cruise", "vetically", [1,1]) }
+  let(:submarine_v) { Boat.new("Submarine", "vertically", [1,1]) }
+  let(:destroyer_v) { Boat.new("Destroyer", "vertically", [1,1]) }
+  let(:cruise_v)    { Boat.new("Cruise", "vertically", [1,1]) }
+
+  let(:submarine_h) { Boat.new("Submarine", "horizontally", [1,1]) }
+  let(:destroyer_h) { Boat.new("Destroyer", "horizontally", [1,1]) }
+  let(:cruise_h)    { Boat.new("Cruise", "horizontally", [1,1]) }
 
   describe '::initialize' do
     
     context 'initializes a Boat' do
-      it { expect(submarine).to be_equal Boat.new("Submarine", "vetically", [1,1]) }
-      it { expect(destroyer).to be_equal Boat.new("Destroyer", "vetically", [1,1]) }
-      it { expect(cruise).to be_equal Boat.new("Cruise", "vetically", [1,1]) }
+      it { expect(submarine_v).to be_equal Boat.new("Submarine", "vertically", [1,1]) }
+      it { expect(destroyer_v).to be_equal Boat.new("Destroyer", "vertically", [1,1]) }
+      it { expect(cruise_v).to be_equal Boat.new("Cruise", "vertically", [1,1]) }
     end
   end
+
+  describe '#positions?' do 
+    it { expect(submarine_v.positions?).to eq [[1,1]] }
+    #it { expect(destroyer_v.positions?).to eq [[1,1],[1,2],[1,3]]}
+    #it { expect(cruise_v.positions?).to eq [[1,1],[1,2]] }
+
+    it { expect(submarine_h.positions?).to eq [[1,1]] }
+    #it { expect(destroyer_h.positions?).to eq [[1,1],[2,1],[3,1]] }
+    #it { expect(cruise_h.positions?).to eq [[1,1],[2,1]] }
+  end
+
 end
 
