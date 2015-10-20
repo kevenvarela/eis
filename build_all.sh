1293604e58acf4e255b214a276671a19779f92aa
GianFF <gf.fioriello@gmail.com>
2015-10-20 20:02:09 -0300

    Adding build all

--- build_all.sh
+++ build_all.sh
@@ -11,3 +11,11 @@ cd ..
 cd kata_2_pptom
 bundle install
 bundle exec rake
+cd ..
+cd kata_3_batallanaval
+bundle install
+bundle exec rake
+cd ..
+cd kata_4_calculadora
+bundle install
+bundle exec rake
